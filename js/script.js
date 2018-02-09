e4034d6d7496c94c7ce176806abbc6fa06437c90
Meagan Moore <816moore@gmail.com>
2018-02-09 16:54:55 -0500

    more javascript. all functional, trying to get drag and drop working

--- js/script.js
+++ js/script.js
@@ -67,81 +67,103 @@ potionApp.potionIngredients = {
 
 }
 
-
-$('.brewIt').click(function () {
-    $('.potionResult').fadeToggle();
-})
-$(document).mouseup(function (e) {
-    var container = $('.potionResult');
-    if (!container.is(e.target)
-        && container.has(e.target).length === 0) {
-        container.fadeOut();
-    }
-});
-
-$('.again').click(function (){
-    // container.fadeOut();
-    location.reload();
-});
-
-$(function() {
-    let Ing1;
-    let Ing2;
-    let Ing3;
-    let prop1;
-    let prop2;
-    let prop3;
-
-    $('form').on('submit', function(event) {
-        event.preventDefault();
-
-        Ing1 = $('input[name=potionIng1]:checked').val();
-        prop1 = potionApp.potionIngredients.herbs[Ing1].property;
-
-        Ing2 = $('input[name=potionIng2]:checked').val();
-        prop2 = potionApp.potionIngredients.components[Ing2].property;
-
-        Ing3 = $('input[name=potionIng3]:checked').val();
-        prop3 = potionApp.potionIngredients.finishingTouch[Ing3].property;
-
-        potionApp.resultSentence = `A new and gently glowing potion sits in your hands. It will grant you ${prop1}. You may also experience ${prop2}. Be warned, ${prop3} is a common side effect of ${Ing1}. Enjoy!`;
-
-        $('.resultWords').html(`<p>${potionApp.resultSentence}</p>`);
-
+//this makes the modal show up
+potionApp.toggleModal = function(){
+    $('.brewIt').click(function () {
+        $('.potionResult').fadeToggle();
+    })
+    $(document).mouseup(function (e) {
+        var container = $('.potionResult');
+        if (!container.is(e.target)
+            && container.has(e.target).length === 0) {
+            container.fadeOut();
+        }
     });
+}
+
+potionApp.startAgain = function() {
+    $('.again').click(function (){
+        // container.fadeOut();
+        location.reload();
+    });
+}
+
+potionApp.displayData = function(prop1, prop2, prop3, prop4) {
+    potionApp.resultSentence = `A new and gently glowing potion sits in your hands. It will grant you ${prop1}. You may also experience ${prop2}. Be warned, ${prop3} is a common side effect of ${prop4}. Enjoy!`;
+
+    $('.resultWords').html(`<p>${potionApp.resultSentence}</p>`);
+}
+//if you click anything outside of the modal, modal disappears
+
+
+//refresh the page on 'brew again' button 
+
+potionApp.handleSubmit = function() {
+
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        
+        const Ing1 = $('input[name=potionIng1]:checked').val();
+        const prop1 = potionApp.potionIngredients.herbs[Ing1].property;
+        
+        const Ing2 = $('input[name=potionIng2]:checked').val();
+        const prop2 = potionApp.potionIngredients.components[Ing2].property;
+        
+        const Ing3 = $('input[name=potionIng3]:checked').val();
+        const prop3 = potionApp.potionIngredients.finishingTouch[Ing3].property;
+        
+        potionApp.displayData(prop1, prop2, prop3, Ing1);
+          
+    });
+}
+
+potionApp.init = function() {
+    potionApp.toggleModal();
+    potionApp.startAgain();
+    potionApp.handleSubmit();
+}
+$(function() {
+    potionApp.init();
+    let dragCheck = false;
+
+
+    $('.draggable').draggable({
+        revert: 'invalid',
+    });
+
+    $('.droppable').droppable({
+        drop: function(e, option) {
+            dragCheck = true;
+
+            console.log(option.draggable[0].dataset.option);
+            const herbs = option.draggable[0].dataset.category;
+
+            
+        }
+    });
+
+    if(dragCheck ===  true) {
+        option.draggable[0] = attr(':checked')
+    }
+ 
 });
 
-
-// potionApp.init = function () {
-//     potionApp.brewPopUp();
+// potionApp.dragCheck = function() {
+//     $('.draggable').on('drag', function(){
+//         $(this).html(".attr('checked')");
+//     })
 // }
 
-// $(document).ready(function (){
-//     potionApp.init();
-// });
-// $(function () {
-//     $('.brewIt').click(function () {
-//         $('.potionResult').fadeToggle();
-//     })
-//     $(document).mouseup(function (e) {
-//         var container = $('.potionResult');
-//         if (!container.is(e.target)
-//             && container.has(e.target).length === 0) {
-//             container.fadeOut();
-//         }
-//     });
-// });
+// potionApp.dragCheck();
+// $('.draggable').on(dragend) {
+//     $('.draggable').html('.attr(`checked`)');
+// }
 
 
 
-$('.draggable').dragabble({
-    cancel: false,
-    revert: true,
-});
-$('.droppable').droppable({
-    accept: ".draggable",
-    tolerance: "touch"
-});
+
+
+
 
 
 
